caeaa16f57a87516d128cfef176b1df278dc5381
TegWorkSite-Hub <tegworksite@yahoo.com>
2025-01-24 14:26:40 -0600

    Update Script_TW.py

--- Script_TW.py
+++ Script_TW.py
@@ -1,14 +1,18 @@
 import yt_dlp
 
 def download_audio(url, download_path='./'):
+    # Definir las opciones para yt-dlp
     ydl_opts = {
         'format': 'bestaudio/best',  # Descargar solo el mejor audio disponible
-        'outtmpl': f'{download_path}%(title)s.%(ext)s',  # Establece el nombre del archivo de salida
+        'outtmpl': f'{download_path}%(title)s.%(ext)s',  # Nombre del archivo de salida
         'postprocessors': [{
             'key': 'FFmpegAudioConvertor',  # Usamos FFmpeg para convertir el audio
             'preferredcodec': 'mp3',        # Convertimos a MP3
             'preferredquality': '192',      # Calidad del MP3
         }],
+        'postprocessor_args': [
+            '-strict', '-2'  # Esto ayuda en algunos casos para evitar problemas con ffmpeg
+        ],
     }
 
     try:
