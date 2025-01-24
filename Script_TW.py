8b954b5562d45db3a42cac100286e5e5ba724825
TegWorkSite-Hub <tegworksite@yahoo.com>
2025-01-24 14:14:37 -0600

    Update Script_TW.py

--- Script_TW.py
+++ Script_TW.py
@@ -1,25 +1,27 @@
 import yt_dlp
 
-def download_audio(url):
-    # Definir las opciones para yt-dlp
+def download_audio(url, download_path='./'):
     ydl_opts = {
-        'format': 'bestaudio/best',  # Descargar el mejor audio disponible
-        'outtmpl': '%(title)s.%(ext)s',  # Nombre del archivo de salida
+        'format': 'bestaudio/best',  # Descargar solo el mejor audio disponible
+        'outtmpl': f'{download_path}%(title)s.%(ext)s',  # Establece el nombre del archivo de salida
         'postprocessors': [{
-            'key': 'FFmpegAudioConvertor',  # Convertir el audio
-            'preferredcodec': 'mp3',        # Convertir a mp3
-            'preferredquality': '192',      # Calidad del mp3
+            'key': 'FFmpegAudioConvertor',  # Usamos FFmpeg para convertir el audio
+            'preferredcodec': 'mp3',        # Convertimos a MP3
+            'preferredquality': '192',      # Calidad del MP3
         }],
     }
 
-    # Intentar descargar el audio
     try:
+        # Intentamos descargar el audio
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
-        print("¡Descarga completada!")
+        print("¡Descarga completada con éxito!")
     except Exception as e:
         print(f"Se produjo un error: {e}")
 
 if __name__ == "__main__":
     url = input("Introduce la URL del video de YouTube: ")
-    download_audio(url)
+    if url:  # Verifica si la URL no está vacía
+        download_audio(url)
+    else:
+        print("Por favor, ingresa una URL válida.")
